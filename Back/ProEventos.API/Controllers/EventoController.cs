2c98f3f86e11e8d85e128227fe67a06ac0751b3d
Alex de Souza Galdino <alexsgaldino@gmail.com>
2024-05-16 16:45:04 -0300

    Introdução ao EF Core

--- Back/ProEventos.API/Controllers/EventoController.cs
+++ Back/ProEventos.API/Controllers/EventoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -7,8 +8,11 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public EventoController()
+        private readonly DataContext _context;
+
+        public EventoController(DataContext context)
         {
+            _context = context;
         }
 
         [HttpPost()]
@@ -20,28 +24,13 @@ namespace ProEventos.API.Controllers
         [HttpGet()]
         public IEnumerable<Evento> Get()
         {
-            return new Evento[]
-            {
-                new Evento()
-                {
-                    Id = 1,
-                    Tema = "Angular e .Net Core",
-                    Local = "Belo Horizonte",
-                    Lote = "1º Lote",
-                    QtdePessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-                },
-                new Evento()
-                {
-                    Id = 2,
-                    Tema = "Angular e Suas Novidades",
-                    Local = "São Paulo",
-                    Lote = "2º Lote",
-                    QtdePessoas = 350,
-                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
-                }
+            return _context.Eventos;
+        }
 
-            };
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _context.Eventos.Where(evento => evento.Id == id);
         }
     }
 }
--- Back/ProEventos.API/Program.cs
+++ Back/ProEventos.API/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using ProEventos.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Add context of project
+builder.Services.AddDbContext<DataContext>(
+    options => options.UseSqlite(builder.Configuration.GetConnectionString("Default"))
+    );
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
